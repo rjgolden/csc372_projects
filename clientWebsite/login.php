3ced45d1d0c13c2b20b0c135a49de4897deecfc1
Ryan Golden <71111462+rjgolden@users.noreply.github.com>
2025-04-30 20:21:32 -0400

    Update login.php

--- clientWebsite/login.php
+++ clientWebsite/login.php
@@ -5,7 +5,7 @@
   // If already logged in
   if ($logged_in) {
     // Redirect to profile page  
-    header('Location: profile.php');
+    header('Location: index.php');
     // Stop further code running
     exit;
   }    
@@ -40,7 +40,7 @@
             // Authentication successful
             login($username);
             // Redirect to profile page
-            header('Location: profile.php');
+            header('Location: index.php');
             exit;
           } else {
             // Password is incorrect
